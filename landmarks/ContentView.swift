bf79810d15f8a134dcb1e57706d4a9995c50f059
Jose García Máñez <jose.garcia@e-valua.es>
2023-06-17 11:49:02 +0200

    initial chantes

--- landmarks/ContentView.swift
+++ landmarks/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Turtle Rock")
+                .font(.title)
+                .foregroundColor(.green)
         }
         .padding()
     }
