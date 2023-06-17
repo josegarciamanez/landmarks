4967b212170f702aa2b4293dab3bd96dbd1ef466
Jose García Máñez <jose.garcia@e-valua.es>
2023-06-17 11:43:33 +0200

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Jose García Máñez on 17/6/23.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
